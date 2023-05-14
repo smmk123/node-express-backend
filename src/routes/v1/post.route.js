976651dc869aa4d5da25cf8bb5d541f4c2bbc404
smmk <smmk123@gmail.com>
2023-05-14 07:07:02 -0400

    fixed post route

--- src/routes/v1/post.route.js
+++ src/routes/v1/post.route.js
@@ -1,3 +1,19 @@
+const express = require('express');
+const validate = require('../../middlewares/validate');
+const auth = require('../../middlewares/auth');
+const postController = require('../../controllers/post.controller');
+const postValidation = require('../../validations/post.validation');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(validate(postValidation.getPosts), postController.getPosts)
+  .post(auth('manageposts'), validate(postValidation.createPost), postController.createPost);
+
+router.route('/:postId').get(validate(postValidation.getPosts), postController.getPost);
+
+module.exports = router;
 /**
  * @swagger
  * tags:
@@ -8,9 +24,9 @@
  * @swagger
  * /posts:
  *   post:
- *     summary: Create a product
+ *     summary: Create a post
  *     description: Only admins can create other users.
- *     tags: [Products]
+ *     tags: [posts]
  *     security:
  *       - bearerAuth: []
  *     requestBody:
@@ -41,7 +57,7 @@
  *         content:
  *           application/json:
  *             schema:
- *                $ref: '#/components/schemas/Product'
+ *                $ref: '#/components/schemas/post'
  *       "400":
  *         $ref: '#/components/responses/DuplicateEmail'
  *       "401":
@@ -50,9 +66,9 @@
  *         $ref: '#/components/responses/Forbidden'
  *
  *   get:
- *     summary: Get all products
- *     description: Only admins can retrieve all products.
- *     tags: [Products]
+ *     summary: Get all posts
+ *     description: Only admins can retrieve all posts.
+ *     tags: [posts]
  *     security:
  *       - bearerAuth: []
  *     parameters:
